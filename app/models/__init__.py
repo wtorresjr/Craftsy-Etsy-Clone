29d733311eb59a23c5676d62af3fd0c9624fd7a2
Tamara <jermanust88@gmail.com>
2023-11-11 16:23:34 -0500

    added product_images model seeder and migration files

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .db import db
 from .user import User
+from .product_image import ProductImage
 from .db import environment, SCHEMA
---
+++ app/models/product_image.py
@@ -0,0 +1,29 @@
+from .db import db, environment, SCHEMA
+from datetime import datetime
+
+
+class ProductImage(db.Model):
+    __tablename__ = "product_images"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(255), nullable=False)
+    preview = db.Column(db.Boolean, nullable=False)
+    # product_id = db.Column(db.Integer, db.ForeignKey(
+    #     "products.id", ondelete="CASCADE"))
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # product = db.relationship("Product", back_populates="product_images")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            # 'product_id': self.product_id,
+            'image_url': self.image_url,
+            'preview': self.preview
+        }
