e8e22c80080e343f85b4e7322e5704c8f8cfde30
Will Torres <w.torresjr@gmail.com>
2023-11-11 13:35:28 -0500

    added routes product reviews routes to products_routes.py

--- app/api/products_routes.py
+++ app/api/products_routes.py
@@ -2,22 +2,47 @@ from flask import Blueprint, jsonify, session, request
 from app.models import User, db
 from app.forms.create_product_form import CreateProductForm
 
-products_routes = Blueprint('products',__name__)
+products_routes = Blueprint('products', __name__)
+
+
+# Get All Products
 
 @products_routes.route('/', methods=['GET'])
 def get_all_products():
-    data = {"Product":"All products route reached"}
+    data = {"Product": "All products route reached"}
     return jsonify(data)
+
+
+# Get Product By Id
 
 @products_routes.route('/<int:product_id>',  methods=['GET'])
 def get_product_details(product_id):
-    data = {"Product":f"Product details for product id: {product_id}"}
+    data = {"Product": f"Product details for product id: {product_id}"}
     return jsonify(data)
+
+
+# Delete Product By Id
+
+@products_routes.route('/<int:product_id>', methods=['DELETE'])
+def delete_product_by_id(product_id):
+    data = {"Product": f"Product {product_id} has been deleted"}
+    return jsonify(data)
+
+# Product Reviews By Id
+
+
+@products_routes.route('/<int:product_id>/reviews', methods=['GET'])
+def get_reviews_by_product_id(product_id):
+    data = {"Product Review": f"Product reviews for product id: {product_id}"}
+    return jsonify(data)
+
+# Create New Product
 
 
 @products_routes.route('/', methods=['POST'])
 def create_new_product():
-    print('Post route reached <==========================')
-    form = CreateProductForm()
 
-    return 1
+    data = request.get_json()
+
+    # Returning request body for testing.
+    return jsonify(data)
