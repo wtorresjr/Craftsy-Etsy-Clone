c83f59f6142806114028d8346dcd7ca082df9b01
Will Torres <w.torresjr@gmail.com>
2023-11-11 18:29:06 -0500

    Added product image routes, review image routes and completed shopping cart routes.

--- app/api/favorites_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-
-favorites_routes = Blueprint('favorites', __name__)
-
-
-@favorites_routes.route('/', methods=['GET'])
-def favorites_test():
-    data = {"Test": "Route works"}
-    return jsonify(data)
--- app/api/shopping_cart_routes.py
+++ app/api/shopping_cart_routes.py
@@ -11,10 +11,33 @@ def get_shopping_cart(cart_id):
 
     return jsonify(data)
 
+# Add to Shopping Cart for Current User
+
 
 @shopping_cart_routes.route('/<int:cart_id>', methods=['POST'])
 def add_item_to_cart(cart_id):
-    #Use cart_id to populate cart_id in json object when posting to table.
+    # Use cart_id to populate cart_id in json object when posting to table.
     data = request.get_json()
-    
+
     return jsonify(data)
+
+
+# Delete from cart
+
+@shopping_cart_routes.route('/<int:cart_id>/cart_items/<int:cart_items_id>', methods=['DELETE'])
+def remove_item_from_cart(cart_id, cart_items_id):
+    data = {
+        "Delete From Cart": f"Removed item: {cart_items_id} from cart number: {cart_id}"}
+    return jsonify(data)
+
+# Edit Cart Quantity
+
+@shopping_cart_routes.route('/<int:cart_id>/cart_items/<int:cart_items_id>', methods=['PUT'])
+def edit_shopping_cart(cart_id, cart_items_id):
+    item_changed = request.get_json()
+    data = {
+        "Edit Route": f"Editing item #{cart_items_id} in cart #{cart_id}, {item_changed}"}
+    return jsonify(data)
+
+
+#Purchase Items in 
