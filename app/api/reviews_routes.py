b39a3e202426caa6a8201a2a8a20c190d0136573
ian <iankimmmmm@gmail.com>
2023-11-13 20:59:26 -0800

    both creating and deleting Review image works properly

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, session, request
 from ..models import Review, ReviewImage, db
+from flask_login import current_user, login_required
 
 reviews_routes = Blueprint('reviews', __name__)
 
@@ -8,16 +9,16 @@ reviews_routes = Blueprint('reviews', __name__)
 
 @reviews_routes.route('/<int:review_id>', methods=['PUT'])
 def edit_review_by_id(review_id):
-    current_review = Review.query.get(review_id)
+  current_review = Review.query.get(review_id)
 
-    data = request.get_json()
+  data = request.get_json()
 
-    current_review.review = data.get('review')
-    current_review.star_rating = data.get('stars')
+  current_review.review = data.get('review')
+  current_review.star_rating = data.get('stars')
 
-    db.session.commit()
+  db.session.commit()
 
-    return current_review.to_dict()
+  return current_review.to_dict()
 
 
 # Delete a Review By Review Id
@@ -25,16 +26,16 @@ def edit_review_by_id(review_id):
 
 @reviews_routes.route('/<int:review_id>', methods=['DELETE'])
 def delete_review_by_id(review_id):
-    current_review = Review.query.get(review_id)
+  current_review = Review.query.get(review_id)
 
-    if current_review:
-        ReviewImage.query.filter_by(review_id = review_id).delete()
+  if current_review:
+    ReviewImage.query.filter_by(review_id = review_id).delete()
 
-        db.session.delete(current_review)
-        db.session.commit()
-        return jsonify({"message": "Successfully deleted"})
-    else:
-        return jsonify({"message": "Review couldn't be found"})
+    db.session.delete(current_review)
+    db.session.commit()
+    return jsonify({"message": "Successfully deleted"})
+  else:
+    return jsonify({"message": "Review couldn't be found"})
 
 
 # Add A Review Image
@@ -42,8 +43,19 @@ def delete_review_by_id(review_id):
 
 @reviews_routes.route('/<int:review_id>/images', methods=['POST'])
 def add_review_image(review_id):
-    data = request.get_json()
-    return jsonify(data)
+  data = request.get_json()
+
+  current_review = Review.query.get(review_id)
+
+  if current_review:
+     new_image = ReviewImage(review_id = review_id, image_url = data.get("image_url"))
+
+     db.session.add(new_image)
+     db.session.commit()
+
+     return new_image.to_dict()
+  else:
+    return jsonify({"message": "Review couldn't be found"})
 
 
 # Delete A Review Image
@@ -51,5 +63,12 @@ def add_review_image(review_id):
 
 @reviews_routes.route('/<int:review_id>/images/<int:image_id>', methods=['DELETE'])
 def delete_review_image(review_id, image_id):
-    data = {"message": f"Successfully deleted the image #{image_id} belonging to product #{review_id}"}
-    return jsonify(data)
+  current_review = Review.query.get(review_id)
+  current_image = ReviewImage.query.get(image_id)
+
+  if current_review and current_image:
+    db.session.delete(current_image)
+    db.session.commit()
+    return jsonify({"message": "Review image deleted successfully."})
+  else:
+    return jsonify({"message": "No review image by that id."})
