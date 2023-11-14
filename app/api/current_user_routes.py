6f67ec93c67705bbdf0755017861f2ad46144096
Krystal Kimmel <kabkimmel@gmail.com>
2023-11-14 01:32:01 -0600

    Modified Get all curent users reviews route

--- app/api/current_user_routes.py
+++ app/api/current_user_routes.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, jsonify, session, request
+from app.models import (Review, User, db)
+from flask_login import login_required, current_user, LoginManager
 
 current_user_routes = Blueprint('current-user', __name__)
 
@@ -6,23 +8,41 @@ current_user_routes = Blueprint('current-user', __name__)
 # Get Current User Info
 
 @current_user_routes.route('/', methods=['GET'])
+@login_required
 def get_current_user_info():
-    data = {"User info": "This is user info"}
-    return jsonify(data)
+    # curr_user_info = User.query.filter_by(email=request.get_json())
+     if current_user.is_authenticated:
+        print(current_user.to_dict())
+        return current_user.to_dict()
+     else:
+         return {'user': 'null'}
+
 
 
 # Get All Current Users Reviews
 
 @current_user_routes.route('/reviews', methods=['GET'])
+@login_required
 def get_current_user_reviews():
-    data = {"Current User Reviews": "All users reviews reached"}
-    return jsonify(data)
+    current_user_id = current_user.id
+
+    current_user_reviews = Review.query.filter_by(user_id=current_user_id).all()
+
+    if not current_user_reviews:
+        return jsonify({"message": "You have not created any reviews."
+    })
+
+    reviews_list = [review.to_dict() for review in current_user_reviews]
+    return jsonify(reviews_list)
+
+
 
 ## Krystals Code....
 # View Current User Favorites
 
 
 @current_user_routes.route('/favorites', methods=['GET'])
+@login_required
 def get_curr_user_favorites():
     data = {"Favorites": "Current user favorites here"}
     return jsonify(data)
@@ -32,6 +52,7 @@ def get_curr_user_favorites():
 
 
 @current_user_routes.route('/favorites', methods=['POST'])
+@login_required
 def add_to_favorites():
     data = request.get_json()
     return jsonify(data)
@@ -41,8 +62,7 @@ def add_to_favorites():
 
 
 @current_user_routes.route('/favorites/<int:favorite_id>', methods=['DELETE'])
+@login_required
 def delete_a_favorite(favorite_id):
     data = {"Message": f"Successfully deleted favorite with an id of {favorite_id}"}
     return jsonify(data)
-
-
