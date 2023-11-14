f7b774e75ae9cc65e73329d929c461dfbe611d80
ian <iankimmmmm@gmail.com>
2023-11-13 20:18:38 -0800

    edit a review by review id complete

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -1,15 +1,23 @@
 from flask import Blueprint, jsonify, session, request
+from ..models import Review, db
 
 reviews_routes = Blueprint('reviews', __name__)
 
 
 # Edit a Review By Review Id
 
-
 @reviews_routes.route('/<int:review_id>', methods=['PUT'])
 def edit_review_by_id(review_id):
+    current_review = Review.query.get(review_id)
+
     data = request.get_json()
-    return jsonify(data)
+
+    current_review.review = data.get('review')
+    current_review.star_rating = data.get('stars')
+
+    db.session.commit()
+
+    return current_review.to_dict()
 
 
 # Delete a Review By Review Id
