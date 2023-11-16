ed702400cf28820357abea5e21b9d18746bf20f9
Will Torres <w.torresjr@gmail.com>
2023-11-16 00:31:03 -0500

    removed db.column from users_id relationship

--- app/models/product.py
+++ app/models/product.py
@@ -15,21 +15,18 @@ class Product(db.Model):
     quantity = db.Column(db.Integer, nullable=False, default=1)
     price = db.Column(db.Float, nullable=False)
 
-    user_id = db.Column(db.Integer, db.ForeignKey(db.Column('users.id')))
-
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-
-    ## RELATIONSHIPS
+    # RELATIONSHIPS
     user = db.relationship("User", back_populates="products")
     favorites = db.relationship("Favorite", back_populates="products")
     product_images = db.relationship('ProductImage', back_populates="products")
     cartitems = db.relationship('CartItem', back_populates="products")
     reviews = db.relationship('Review', back_populates='products')
-
 
     def to_dict(self):
         return {
