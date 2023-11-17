7858bb112ce5e429449cbb48e5b1a5e7220c4b51
Will Torres <w.torresjr@gmail.com>
2023-11-16 23:50:48 -0500

    added comments to products reducer

---
+++ react-app/src/store/products.js
@@ -0,0 +1,48 @@
+
+
+//Get all Products
+
+//Get Product By ID
+
+//Delete a Product by ID
+
+//Get Product Reviews By Product ID
+
+//Create A New Product
+
+
+
+
+
+
+
+
+//Edit a Product
+
+
+//Create a Product Review By Product ID
+
+
+//Get All Products Created By Current User
+
+
+//Add a Product Image
+
+
+//Delete A Product Image
+
+
+const initialState = {};
+
+
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case :
+      return {  };
+    case :
+      return { };
+    default:
+      return state;
+  }
+}
