01f5aafbcf0c8bd5c810689294e52f4a8d6d7f4b
Will Torres <w.torresjr@gmail.com>
2023-11-17 01:08:26 -0500

    Added product-edit and getUserCreatedProducts thunks

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,6 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserProducts } from "../../store/products";
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+  const userCreatedProducts = useSelector(
+    (state) => state?.products?.userCreatedProducts
+  );
+
+  useEffect(() => {
+    dispatch(getUserProducts);
+  });
+
   return (
     <>
       <h1>Home Page (Products page)</h1>
--- react-app/src/store/products.js
+++ react-app/src/store/products.js
@@ -1,5 +1,3 @@
-
-
 //Get all Products
 
 //Get Product By ID
@@ -10,38 +8,72 @@
 
 //Create A New Product
 
-
-
-
-
-
-
-
 //Edit a Product
+const EDIT_PRODUCT = "products/EDIT_PRODUCT";
+const editProduct = (editedProduct) => ({
+  type: EDIT_PRODUCT,
+  payload: editedProduct,
+});
 
+export const editAproduct =
+  (product_id, name, description, price, preview_image_url) =>
+  async (dispatch) => {
+    try {
+      const response = await fetch(`/api/products/${product_id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          description,
+          price,
+          preview_image_url,
+        }),
+      });
+
+      const edited = await response.json();
+      dispatch(editProduct(edited));
+      return edited;
+    } catch (error) {
+      throw error;
+    }
+  };
 
 //Create a Product Review By Product ID
 
-
 //Get All Products Created By Current User
+const GET_PRODUCTS_BY_USER = "products/GET_PRODUCTS_BY_USER";
+const getProductsByUser = (userProducts) => ({
+  type: GET_PRODUCTS_BY_USER,
+  payload: userProducts,
+});
 
+export const getUserProducts = () => async (dispatch) => {
+  try {
+    const response = await fetch("/api/products/current-user", {
+      method: "GET",
+    });
+    const foundUserProducts = await response.json();
+    dispatch(getProductsByUser(foundUserProducts));
+    return foundUserProducts;
+  } catch (error) {
+    throw error;
+  }
+};
 
 //Add a Product Image
 
-
 //Delete A Product Image
-
 
 const initialState = {};
 
-
-
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case :
-      return {  };
-    case :
-      return { };
+    case EDIT_PRODUCT:
+      return { ...state, productEdited: action.editedProduct };
+    case GET_PRODUCTS_BY_USER:
+      return { ...state, userCreatedProducts: action.userProducts };
     default:
       return state;
   }
