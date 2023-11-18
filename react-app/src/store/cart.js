b765054112d5c1d8f1e4d341b8a42f875cecf99e
Daniel Calderon <danielcalderon1199@gmail.com>
2023-11-17 19:09:28 -0800

    Adding 'add item to cart' (State)

--- react-app/src/store/cart.js
+++ react-app/src/store/cart.js
@@ -1,8 +1,14 @@
 const LOAD_CART_ITEMS = "session/LOAD_CART_ITEMS";
+const ADD_CART_ITEM = "session/ADD_CART_ITEM";
 
 const loadCartItems = (items) => ({
 	type: LOAD_CART_ITEMS,
 	payload: items
+});
+
+const addCartItem = (item) => ({
+    type: ADD_CART_ITEM,
+    payload: item
 });
 
 
@@ -26,10 +32,39 @@ export const getCart = (cartId) => async (dispatch) => {
     }
 }
 
+export const addItem = (itemData, cartId) => async (dispatch) => {
+    const options = {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(itemData)
+    }
+    const response = await fetch(`/api/cart/${+cartId}`, options);
+
+    if (response.ok) {
+        const newItem = await response.json()
+        dispatch(addCartItem(newItem));
+        dispatch(getCart(+cartId));
+        return newItem;
+    } else if (response.status === 404) {
+        return [];
+    }
+}
+
 const cartReducer = (state = initialState, action) => {
+    let newState;
     switch (action.type) {
         case LOAD_CART_ITEMS:
-            return { ...state, ...action.payload };
+            if (action.payload.cart) {
+                action.payload.cart.items.forEach((item) => newState[item.id] = item)
+                return newState;
+            }
+            else {
+                newState = action.payload
+                return newState;
+            }
+        case ADD_CART_ITEM:
+            newState = { ...state, [action.payload.id]: action.payload }
+            return newState;
         default:
             return state;
     }
