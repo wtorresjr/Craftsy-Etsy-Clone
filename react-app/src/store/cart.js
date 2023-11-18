c6b0838c21c48f62b8b711c827f4b6abdeb42fb4
Daniel Calderon <danielcalderon1199@gmail.com>
2023-11-18 12:37:31 -0800

    Adding try catch blocks to code

--- react-app/src/store/cart.js
+++ react-app/src/store/cart.js
@@ -27,69 +27,83 @@ const initialState = {}
 
 
 export const getCart = () => async (dispatch) => {
-    const response = await fetch(`/api/cart`);
-
-    if (response.ok) {
-        const cartItems = await response.json();
-        const items = cartItems.Cart.reduce((acc, obj) => {
-            acc[obj.id] = obj;
-            return acc;
-        }, {});
-        const newState = {'items': items}
-        dispatch(loadCartItems(newState));
-        return newState;
-    } else if (response.status === 404) {
-        return [];
+    try {
+        const response = await fetch(`/api/cart`);
+        if (response.ok) {
+            const cartItems = await response.json();
+            const items = cartItems.Cart.reduce((acc, obj) => {
+                acc[obj.id] = obj;
+                return acc;
+            }, {});
+            const newState = {'items': items}
+            dispatch(loadCartItems(newState));
+            return newState;
+        }
+    } catch (res) {
+        let errors = await res.json();
+        return errors;
     }
+
+
 }
 
 export const addItem = (itemData, cartId) => async (dispatch) => {
-    const options = {
-        method: 'POST',
-        headers: {'Content-type': 'application/json'},
-        body: JSON.stringify(itemData)
-    }
-    const response = await fetch(`/api/cart/${+cartId}`, options);
+    try {
+        const options = {
+            method: 'POST',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify(itemData)
+        }
+        const response = await fetch(`/api/cart/${+cartId}`, options);
 
-    if (response.ok) {
-        const newItem = await response.json()
-        dispatch(addCartItem(newItem));
-        dispatch(getCart(+cartId));
-        return newItem;
-    } else if (response.status === 404) {
-        return [];
+        if (response.ok) {
+            const newItem = await response.json()
+            dispatch(addCartItem(newItem));
+            dispatch(getCart(+cartId));
+            return newItem;
+        }
+
+    } catch (res) {
+        let errors = await res.json();
+        return errors;
     }
 }
 
 export const deleteItem = (cartId, cartItemId) => async (dispatch) => {
-    const response = await fetch(`/api/cart/${cartId}/cart_items/${cartItemId}`, {
-        method: "DELETE"
-    });
+    try {
+        const response = await fetch(`/api/cart/${cartId}/cart_items/${cartItemId}`, {
+            method: "DELETE"
+        });
 
-    if (response.ok) {
-        const item = await response.json();
-        dispatch(deleteCartItem((item)));
-        dispatch(getCart(+cartId));
-        return item
-    } else if (response.status === 404) {
-        return [];
+        if (response.ok) {
+            const item = await response.json();
+            dispatch(deleteCartItem((item)));
+            dispatch(getCart(+cartId));
+            return item
+        }
+    } catch (res) {
+        let errors = await res.json();
+        return errors;
     }
 }
 
 export const editItem = (itemData, cartId, cartItemId) => async (dispatch) => {
-    const response = await fetch(`/api/cart/${cartId}/cart_items/${cartItemId}`, {
-        method: "PUT",
-        headers: {"Content-type": "application/json"},
-        body: JSON.stringify(itemData)
-    })
+    try {
+        const response = await fetch(`/api/cart/${cartId}/cart_items/${cartItemId}`, {
+            method: "PUT",
+            headers: {"Content-type": "application/json"},
+            body: JSON.stringify(itemData)
+        })
 
-    if (response.ok) {
-        const item = await response.json();
-        dispatch(editCartItem());
-        dispatch(getCart(+cartId));
-        return item
-    } else if (response.status === 404) {
-        return [];
+        if (response.ok) {
+            const item = await response.json();
+            dispatch(editCartItem());
+            dispatch(getCart(+cartId));
+            return item
+        }
+    } catch (res) {
+        let errors = res.json();
+        return errors;
     }
 }
 
--- react-app/src/store/orders.js
+++ react-app/src/store/orders.js
@@ -11,29 +11,30 @@ const initialState = {}
 
 
 export const getOrders = () => async (dispatch) => {
-    const response = await fetch(`/api/cart/orders`);
+    try {
+        if (response.ok) {
+            const cartItems = await response.json();
+            const ordersObject = cartItems.Orders.reduce((acc, order) => {
+                // Transform each order.items array into an object with a specific key
+                const itemsObject = order.items.reduce((itemsAcc, item) => {
+                    itemsAcc[item.id] = item;
+                    return itemsAcc;
+                }, {});
 
-    if (response.ok) {
-        const cartItems = await response.json();
-        const ordersObject = cartItems.Orders.reduce((acc, order) => {
-            // Transform each order.items array into an object with a specific key
-            const itemsObject = order.items.reduce((itemsAcc, item) => {
-                itemsAcc[item.id] = item;
-                return itemsAcc;
+                // Assign the items object to the order using the cart_id as the key
+                acc[order.cart_id] = { ...order, items: itemsObject };
+
+                return acc;
             }, {});
+            dispatch(loadOrders(ordersObject));
 
-            // Assign the items object to the order using the cart_id as the key
-            acc[order.cart_id] = { ...order, items: itemsObject };
-
-            return acc;
-        }, {});
-        dispatch(loadOrders(ordersObject));
-
-
-        return cartItems;
-    } else if (response.status === 404) {
-        return [];
+            return cartItems;
+        }
+    } catch (res) {
+        let errors = res.json();
+        return errors;
     }
+    const response = await fetch(`/api/cart/orders`);
 }
 
 const orderReducer = (state = initialState, action) => {
