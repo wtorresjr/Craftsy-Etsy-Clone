c99a3dc8053112abeb7e2455ae9aa8547379d67e
ian <iankimmmmm@gmail.com>
2023-11-20 20:03:20 -0800

    review store ready for testing

--- react-app/src/store/reviews.js
+++ react-app/src/store/reviews.js
@@ -4,6 +4,8 @@ const GET_REVIEW_PRODUCTID = "review/GET_REVIEW_PRODUCTID"
 const SET_REVIEW = "review/SET_REVIEW"
 const UPDATE_REVIEW = "review/UPDATE_REVIEW"
 const REMOVE_REVIEW = "review/REMOVE_REVIEW"
+const SET_REVIEW_IMAGE = "review/SET_REVIEW_IMAGE"
+const REMOVE_REVIEW_IMAGE = "review/REMOVE_REVIEW_IMAGE"
 
 const getReview = (reviews) => ({
   type: GET_REVIEW,
@@ -25,11 +27,20 @@ const updateReview = (review) => ({
   payload: review
 })
 
-const removeReview = () => ({
+const removeReview = (reviewId) => ({
   type: REMOVE_REVIEW,
+  payload: reviewId
 })
 
+const setReviewImage = (reviewImage) => ({
+  type: SET_REVIEW_IMAGE,
+  payload: reviewImage
+})
 
+const removeReviewImage = (reviewImageId) => ({
+  type: REMOVE_REVIEW_IMAGE,
+  payload: reviewImageId
+})
 
 // Get all Reviews
 export const fetchReviews = () => async (dispatch) => {
@@ -114,7 +125,7 @@ export const deleteReview = (reviewId) => async (dispatch) => {
       },
     })
     if (response.ok) {
-      dispatch(removeReview())
+      dispatch(removeReview(reviewId))
     }
   }
   catch(error) {
@@ -122,12 +133,58 @@ export const deleteReview = (reviewId) => async (dispatch) => {
   }
 }
 
-const initialState = { review: null };
+// create a review image
+export const createReviewImage = (reviewId, imageData) => async (dispatch) => {
+  try {
+    const response = await fetch(`/api/reviews/${reviewId}/images`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(imageData)
+    })
+    if (response.ok) {
+      const newImage = await response.json();
+      dispatch(setReviewImage(newImage))
+      return newImage
+    }
+  }
+  catch (error) {
+    throw error
+  }
+}
+
+// delete a review image
+export const deleteReviewImage = (reviewId, imageId) => async (dispatch) => {
+  try {
+    const response = await fetch(`/api/reviews/${reviewId}/images/${imageId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+    if (response.ok) {
+      dispatch(removeReviewImage())
+    }
+  }
+  catch(error) {
+    throw error;
+  }
+}
+
+const initialState = {
+  allReviews: [],
+  allReviewsById: [],
+  reviewByProductId: [],
+  allReviewImages: [],
+};
 
 export default function reducer(state = initialState, action) {
+  let newState = {};
 	switch (action.type) {
 		case GET_REVIEW:
       if (action.payload.Reviews) {
+        newState = JSON.parse(JSON.stringify(state));
         const reviewsById = {};
         action.payload.Reviews.forEach((review) => {
           reviewsById[review.id] = review
@@ -145,6 +202,7 @@ export default function reducer(state = initialState, action) {
 
     case GET_REVIEW_PRODUCTID:
       if (action.payload.Review) {
+        newState = JSON.parse(JSON.stringify(state));
         newState = {
           reviewByProductId: action.payload.Review
         }
@@ -162,8 +220,27 @@ export default function reducer(state = initialState, action) {
       return {...state, ...state.action.payload}
 
     case REMOVE_REVIEW:
+      let newAllReviews = allReviews.filter((review) => review.id != action.payload)
+      let newAllReviewsById = allReviewsById.filter((review) => review.id != action.payload)
       return {
-        ...state
+        ...state,
+        allReviews: newAllReviews,
+        allReviewsById: newAllReviewsById
+      }
+
+    case SET_REVIEW_IMAGE:
+      let newImageState = allReviewImages
+      newImageState[action.payload.id] = action.payload
+      return {
+        ...state,
+        allReviewImages: newImageState
+      }
+
+    case REMOVE_REVIEW_IMAGE:
+      let newDeletedImageState = allReviewImages.filter((reviewImage) => reviewImage.id != action.payload)
+      return {
+        ...state,
+        allReviewImages: newDeletedImageState
       }
 
 		default:
