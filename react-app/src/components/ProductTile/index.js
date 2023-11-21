ea3592684569a5b85978c722ce3e866ad18635b9
Will Torres <w.torresjr@gmail.com>
2023-11-20 23:29:39 -0500

    added actual images to product_images seed and review images seed

--- react-app/src/components/ProductTile/index.js
+++ react-app/src/components/ProductTile/index.js
@@ -8,6 +8,7 @@ const ProductTile = ({ product }) => {
         <p>{product.description}</p>
         <div className="priceContainer">${product.price}</div>
         <p>Number of Images: {product.Product_Images.length}</p>
+        <img src={product.preview_image_url} />
       </div>
     </>
   );
