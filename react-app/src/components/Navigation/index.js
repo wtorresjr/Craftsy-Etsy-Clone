cb456cc830397922750360916415013f30dd1f37
Tamara <jermanust88@gmail.com>
2023-11-21 01:55:26 -0500

    added nav-bar options content, styling still in progress

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
+		<>
+			<div className='navBar'>
+				<div className="homeButtonDiv">
+					<NavLink exact to="/" className="homeButton">Craftsy</NavLink>
+				</div>
+				<div className='categoriesDiv'>
+					<Link className='categories'>
+						<i className="fas fa-bars"> </i>
+						<span> Categories</span>
+					</Link>
+				</div>
+				<div className='searchBarDiv'>
+					<div className='searchBar'>
+						<input className='searchBarInput' placeholder='Search for anything' />
+						<i className="fas fa-search" />
+					</div>
+				</div>
+				{isLoaded && (
+					<div className="signInDiv">
+						<ProfileButton user={sessionUser} />
+					</div>
+				)}
+				<div className='shoppingCartDiv'>
+					<NavLink to="/shoppingcart" className='shoppingCart'>
+						<i class="fas fa-shopping-cart"></i>
+					</NavLink>
+				</div>
+			</div>
+		</>
 	);
 }
 
