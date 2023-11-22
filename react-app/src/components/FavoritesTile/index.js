e470bbf566bed1fff599dc0139d8af93dc7d99d4
Krystal Kimmel <kabkimmel@gmail.com>
2023-11-21 23:14:29 -0600

    Added FavoritesTile folder to Components folder

---
+++ react-app/src/components/FavoritesTile/index.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+
+function FavoritesTile ({ favorite, favorited, changeState }) {
+    return (
+        <div className="tileContainer">
+            <div className="heartFav"  onClick={() => changeState(favorite.id)}>
+                {favorited[favorite.id] ? <i className="fas fa-heart" style={{ color: "#c70000" }} key={`heartFav-${favorite.id}`}></i> : <i className="far fa-heart" style={{ color: "black" }}></i>}
+            </div>
+            <div className="favorite-product-preview-img" title={favorite.name}>
+                <img src={favorite.preview_image_url[0]} alt={favorite.name}/>
+            </div>
+            <div className="favorite-product-description">
+                <ul>
+                    <li>{favorite.name}</li>
+                    <li style={{'fontWeight': '600'}}>${favorite.price.toFixed(2)}</li>
+                    {favorite.id % 2 === 0 && <li className="free-shipping-label">FREE Shipping</li>}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default FavoritesTile;
