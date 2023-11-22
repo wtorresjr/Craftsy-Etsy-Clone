4045c8c5bec4eb7470f33a96592cfb3403328fcb
Krystal Kimmel <kabkimmel@gmail.com>
2023-11-21 23:06:38 -0600

    Updated file so it adds the FavoritesTile as a separate component

--- react-app/src/components/FavoritesPage/index.js
+++ react-app/src/components/FavoritesPage/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as favoriteActions from "../../store/favorite";
+import FavoritesTile from "../HomePage/FavoritesTile";
 import "./FavoritesPage.css";
 
 
@@ -22,8 +23,6 @@ function FavoritesPage () {
         setFavorited(initialFavoritedState)
     }, [allFavorites])
 
-
-
     const changeState = (id) => {
         setFavorited(prev=> ({
             ...prev,
@@ -37,36 +36,16 @@ function FavoritesPage () {
 
     return (
         <>
-        {allFavorites &&
-            <div className="favorites-page-container">
-            <h1 style={{'fontWeight':'300'}}>Favorite items <span style={{'color':'#717171', 'fontSize':'0.65em'}}>{allFavorites?.length} items</span></h1>
-            <div className="container-favorite-cards">
-                {allFavorites.map((favorite) => {
-                    return (
-                    <div className="favorite-card" key={`favorite-card-${favorite.id}`}>
-                        <div className="favorite-product-preview-img" title={favorite.name}>
-                            <div
-                                className="favoritesPage-heartFav"
-                                key={`favorite-card-${favorite.id}`}
-                                onClick={() => changeState(favorite.id)}>{favorited[favorite.id] ? <i className="fas fa-heart" style={{ color: "#c70000" }} key={`heartFav-${favorite.id}`}></i> : <i className="far fa-heart" style={{ color: "black" }}></i>}
-                            </div>
-                            <img src={favorite.preview_image_url[0]} alt={favorite.name}/>
-                        </div>
-                        <div className="favorite-product-description">
-                            <ul>
-                                <li>{favorite.name}</li>
-                                <li style={{'fontWeight': '600'}}>${favorite.price.toFixed(2)}</li>
-                                {favorite.id % 2 === 0 && <li className="free-shipping-label">FREE Shipping</li>}
-                            </ul>
-                        </div>
-                    </div>
-                    )
-                })}
+            <div className="favoritespage-container">
+                {allFavorites &&
+                    allFavorites.map((favorite) => {
+                         return <FavoritesTile key={favorite.id} favorite={favorite} favorited={favorited} changeState={changeState} />
+                    })
+                }
+
             </div>
-        </div>
-        }
-    </>
-  )
+        </>
+    )
 }
 
 export default FavoritesPage;
