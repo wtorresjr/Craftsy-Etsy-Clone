7dcdc79c7153b9594ea6b8840a5a48f2c3935116
Will Torres <w.torresjr@gmail.com>
2023-11-27 03:44:02 -0500

    refactored heart component to have bounce effect when favoriting a product

--- react-app/src/components/Product-Components/HeartComponent/index.js
+++ react-app/src/components/Product-Components/HeartComponent/index.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import "../ProductTile/product_img_tile.css";
 
 const FavoriteHeart = () => {
-  return <div className="heartContainer">Heart</div>;
+  const [isClicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!isClicked);
+  };
+
+  return (
+    <div
+      className={`heartContainer ${isClicked ? "clicked" : ""}`}
+      onClick={handleClick}
+    >
+      <i
+        className={`fa-heart ${
+          isClicked ? "fas fa-heart fa-lg" : "far fa-heart"
+        }`}
+      ></i>
+    </div>
+  );
 };
 
 export default FavoriteHeart;
+
+{
+  /* <i className="fas fa-heart fa-lg"></i> */
+}
