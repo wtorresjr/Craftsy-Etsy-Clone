157fa0f4996eda199614e9261b3cedd17b367f45
Will Torres <w.torresjr@gmail.com>
2023-11-27 04:28:49 -0500

    working on dynamic resize of product tiles

--- react-app/src/components/Product-Components/PriceComponent/index.js
+++ react-app/src/components/Product-Components/PriceComponent/index.js
@@ -1,7 +1,7 @@
 import "./price_component.css";
 
 const PriceComponent = ({ product }) => {
-  return <div className="priceDiv">{product?.price}</div>;
+  return <div className="priceDiv">${product?.price}</div>;
 };
 
 export default PriceComponent;
