797c5860d347a6aa48760939ea1e39eeddf5c160
ian <iankimmmmm@gmail.com>
2023-11-27 18:44:02 -0800

    creating review list

--- react-app/src/components/ReviewList/PrintReview.js
+++ react-app/src/components/ReviewList/PrintReview.js
@@ -2,4 +2,36 @@ import 'ReviewList.css'
 
 const PrintReview = ({review}) => {
 
+  let image = ""
+  let name = ""
+
+  if (review.ReviewImages) {
+    image = review.ReviewImages[0].image
+  }
+
+  if (review.User) {
+    name = review.User.firstName + " " + review.User.lastName
+  }
+
+  return(
+    <>
+    <div>
+      {/* gotta add stars to show as stars */}
+      {review.star_rating}
+    </div>
+    <div>
+      {review.review}
+    </div>
+    <div>
+      {/* gotta add conditions where image doesnt exist */}
+      <img className = "reviewImage" src = {image} />
+    </div>
+    <div>
+      {/* need styling */}
+      {name}
+    </div>
+    </>
+  )
 }
+
+export default PrintReview
