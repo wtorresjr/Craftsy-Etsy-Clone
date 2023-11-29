89b61e9f097a7f4dd59d7dfb5906be0a7752d434
Krystal Kimmel <kabkimmel@gmail.com>
2023-11-28 21:33:48 -0600

    fixed paths

--- react-app/src/components/Favorite-Components/FavoritesPage/index.js
+++ react-app/src/components/Favorite-Components/FavoritesPage/index.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-import * as favoriteActions from "../../store/favorite";
+import * as favoriteActions from "../../../store/favorite";
 import FavoritesTile from "../FavoritesTile";
 import NoFavorites from "./NoFavorites";
 import "./FavoritesPage.css";
