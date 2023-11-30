082f71abd12008f79126f07b27b93220717dbb02
Will Torres <w.torresjr@gmail.com>
2023-11-30 01:32:22 -0500

    fixed unterminated jsx content error, causing failed compile

--- react-app/src/components/ProductDetailPage/index.js
+++ react-app/src/components/ProductDetailPage/index.js
@@ -17,8 +17,8 @@ const ProductDetailPage = () => {
 
   const currentProduct = useSelector((state) => state?.products?.productDetail);
   let index = [];
-  for(let i = 1; i < currentProduct.quantity + 1; i++){
-    index.push(i)
+  for (let i = 1; i < currentProduct.quantity + 1; i++) {
+    index.push(i);
   }
 
   useEffect(() => {
@@ -33,46 +33,41 @@ const ProductDetailPage = () => {
   return (
     <>
       <h1>{currentProduct?.name}</h1>
-      {
-        currentProduct?.preview_image_url ?
-          <img src={currentProduct?.preview_image_url[0]} /> : "no image"
-      }
-
-      <div>
-
-        <div className="itemprice">
-          ${currentProduct?.price}
-        </div>
-        <div className="itemdescription">
-          {currentProduct?.description}
-        </div>
       {currentProduct?.preview_image_url ? (
         <img src={currentProduct?.preview_image_url[0]} />
       ) : (
         "no image"
       )}
+
       <div>
         <div className="itemprice">${currentProduct?.price}</div>
         <div className="itemdescription">{currentProduct?.description}</div>
-        <div className="itemarriving">
-          <i class="fa-solid fa-check"></i>
-          Arrives soon! Get it by Tomorrow if you order today
+        {currentProduct?.preview_image_url ? (
+          <img src={currentProduct?.preview_image_url[0]} />
+        ) : (
+          "no image"
+        )}
+        <div>
+          <div className="itemprice">${currentProduct?.price}</div>
+          <div className="itemdescription">{currentProduct?.description}</div>
+          <div className="itemarriving">
+            <i class="fa-solid fa-check"></i>
+            Arrives soon! Get it by Tomorrow if you order today
+          </div>
         </div>
+        <label className="dropdown">Quantity</label>
+        <select id="dropdown" value={selected} onChange={handleSelectChange}>
+          {index.map((idx) => {
+            return <option value={idx}>{idx}</option>;
+          })}
+        </select>
+        <button>Add to Cart</button>
+        <hr />
+        Releated Searches
+        <hr />
+        <ReviewList productId={productId} />
+        <hr />
       </div>
-      <label className="dropdown">Quantity</label>
-      <select id="dropdown" value={selected} onChange={handleSelectChange}>
-        {
-          index.map((idx) => {
-          return (<option value={idx}>{idx}</option>)
-          })
-        }
-      </select>
-      <button>Add to Cart</button>
-      <hr />
-      Releated Searches
-      <hr />
-      <ReviewList productId={productId} />
-      <hr />
     </>
   );
 };
