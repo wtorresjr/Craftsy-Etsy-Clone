ba21d7b0be12a1054754b8459f7903a5872d5fa4
Will Torres <w.torresjr@gmail.com>
2023-12-01 21:25:32 -0500

    Changed image seeder for Product additional images, review images to be static and not random from picsum

--- app/seeds/product_images.py
+++ app/seeds/product_images.py
@@ -8,17 +8,20 @@ def seed_product_images():
 
     for product_id in range(1, 40):
         # Generate 4 images with preview value as false
+        fImage = requests.get(
+            f"https://picsum.photos/600/600.jpg?random={random.randint(501,1001)}")
+        if fImage.status_code == 200:
+            falseImageUrl = fImage.url
         for _ in range(4):
-            false_image_url = f"https://picsum.photos/600/600.jpg?random={random.randint(351,651)}"
             new_false_preview_image = ProductImage(
-                product_id=product_id, image_url=false_image_url, preview=False)
+                product_id=product_id, image_url=falseImageUrl, preview=False)
             db.session.add(new_false_preview_image)
 
         # Generate 1 image with preview true
         image = requests.get(
             f"https://picsum.photos/600/600.jpg?random={random.randint(1,500)}")
-        
-        if image.status_code==200:
+
+        if image.status_code == 200:
             true_image_url = image.url
 
         new_true_preview_image = ProductImage(
