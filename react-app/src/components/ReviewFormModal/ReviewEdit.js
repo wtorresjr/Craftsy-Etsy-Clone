352a3791153897e37fd53a24d0b40ef15bf70be1
Krystal Kimmel <kabkimmel@gmail.com>
2024-01-13 14:21:21 -0600

    Added divs and stars for rating

--- react-app/src/components/ReviewFormModal/ReviewEdit.js
+++ react-app/src/components/ReviewFormModal/ReviewEdit.js
@@ -4,16 +4,26 @@ import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { EditReview } from '../../store/reviews'
 import { useModal } from '../../context/Modal'
+import { useSelector } from 'react-redux';
+import {FaStar} from "react-icons/fa";
+
 
 function ReviewEditModal ({review}) {
   const history = useHistory();
   const dispatch = useDispatch();
   const { closeModal } = useModal();
 
+  const prodInfo = useSelector(state => state.products.productDetail)
+  const currDateObj = new Date()
+  const dateOnly = currDateObj.toISOString().split('T')[0];
+
   const [reviewData, setReviewData] = useState(review.review);
-  const [star_rating, setStar_rating] = useState(review.star_rating);
+  let [stars, setStars] = useState(review.star_rating);
+  const [hover, setHover] = useState(null);
   const [errors, setErrors] = useState({});
   const [isDisabled, setDisabled] = useState(true);
+  const [showErrors, setShowErrors] = useState(false)
+  const submitButtonCN = isDisabled ? 'disabled-button': 'enabled-button'
 
   const errorCollector = {}
 
@@ -25,15 +35,15 @@ function ReviewEditModal ({review}) {
       errorCollector.review = "review has only spaces"
     }
 
-    if(star_rating === ''){
-      errorCollector.stars = "stars is empty"
+    if(!stars){
+      errorCollector.stars = "Star Rating Required"
     }
-    else if(parseInt(star_rating) !== 1 && parseInt(star_rating) !== 2 &&
-    parseInt(star_rating) !== 3 &&
-    parseInt(star_rating) !== 4 &&
-    parseInt(star_rating) !== 5 ) {
-      errorCollector.stars = "invalid input for stars (must be between 1 - 5)"
-    }
+    // else if(parseInt(stars) !== 1 && parseInt(star_rating) !== 2 &&
+    // parseInt(star_rating) !== 3 &&
+    // parseInt(star_rating) !== 4 &&
+    // parseInt(star_rating) !== 5 ) {
+    //   errorCollector.stars = "invalid input for stars (must be between 1 - 5)"
+    // }
 
     setErrors(errorCollector)
     if (Object.keys(errorCollector).length > 0) {
@@ -41,7 +51,7 @@ function ReviewEditModal ({review}) {
     } else {
       setDisabled(false);
     }
-  }, [reviewData, star_rating])
+  }, [reviewData, stars])
 
   // const handleSubmit = async (e) => {
 
@@ -62,7 +72,7 @@ function ReviewEditModal ({review}) {
 
     let newReview = {
       review: reviewData,
-      stars : parseInt(star_rating)
+      stars : stars
     }
     //dispatching to create a review
     await dispatch(EditReview(review.id, newReview))
@@ -79,35 +89,66 @@ function ReviewEditModal ({review}) {
       window.location.reload()
   }
 
-  return(
+  return (
     <>
-      <h1>Edit a Review</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Review
-          <input
-            type="text"
-            value={reviewData}
-            onChange={(e) => setReviewData(e.target.value)}
-            required
-          />
-        </label>
-        {errors && errors.review && <p className="errorDiv">{errors.review}</p>}
-        <label>
-          Stars
-          <input
-            type="text"
-            value={star_rating}
-            onChange={(e) => setStar_rating(e.target.value)}
-            required
-          />
-        </label>
-        {errors && errors.stars && <p className="errorDiv">{errors.stars}</p>}
-        <button type="submit" disabled={isDisabled}>Edit a Review</button>
-      </form>
-    </>
-  )
+    <div className="reviews-modal-wrapper">
+      <h1 className="reviews-modal-h1">Leave a Review</h1>
+      <div className="reviews-modal-top">
+        <div className="reviews-modal-product-img-div">
+          <img src={prodInfo.preview_image_url} alt={prodInfo.name}/>
+        </div>
+        <div className="reviews-modal-product-info-div">
+          <h3>{prodInfo.name}</h3>
+          <p style={{color: "#A3A3A3"}}>Purchased from <span style={{color: "#595959"}}>{prodInfo.Seller.first_name}store</span> on {dateOnly}</p>
+        </div>
+      </div>
 
+        <form onSubmit={handleSubmit}>
+          <div className="reviews-modal-stars-div">
+            <label> My review rating <span style={{color:'#A61A2D'}}>*</span></label>
+            <div className="stars-div">
+              {[...Array(5)].map((star, i) => {
+                  const ratingValue = i + 1;
+                  console.log(stars, ':the current rating')
+                  return (
+                    <label key={i}>
+                        <input
+                            type='radio'
+                            name='rating'
+                            value={ratingValue}
+                            onClick={() => setStars(ratingValue)}
+                            />
+                        <FaStar
+                            className='star'
+                            color={ratingValue <= (hover || stars) ? "#000000" : "#e4e5e9"}
+                            size={30}
+                            style={{cursor:'pointer'}}
+                            onMouseEnter={() => setHover(ratingValue)}
+                            onMouseLeave={() => setHover(null)}
+                            />
+                    </label>
+                    )
+                })}
+            </div>
+            {showErrors && errors?.stars && <p className="errorDiv">{errors.stars}</p>}
+          </div>
+          <div className="reviews-modal-review-div">
+            <label>Your review <span style={{color:'#A61A2D'}}>*</span></label>
+            <textarea
+              value={reviewData}
+              onChange={(e) => setReviewData(e.target.value)}
+              required
+            >
+            </textarea>
+            {showErrors && errors?.review && <p className="errorDiv">{errors.review}</p>}
+          </div>
+          <div className="reviews-modal-submit-button-div">
+            <button type="submit" disabled={isDisabled} className={submitButtonCN}>Update Review</button>
+          </div>
+        </form>
+    </div>
+    </>
+  );
 }
 
 export default ReviewEditModal
