a1cd9e2294a54710dcf1af5a8131b48f70157da1
Krystal Kimmel <kabkimmel@gmail.com>
2024-01-13 13:01:53 -0600

    added divs and some inline styling

--- react-app/src/components/ReviewFormModal/index.js
+++ react-app/src/components/ReviewFormModal/index.js
@@ -1,20 +1,31 @@
 import "./ReviewForm.css";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { createReview, createReviewImage } from "../../store/reviews";
+import { getAllProducts, getProductInfo } from "../../store/products";
 import { useModal } from "../../context/Modal";
+import {FaStar} from "react-icons/fa";
+
 
 function ReviewFormModal({ productId }) {
   const history = useHistory();
   const dispatch = useDispatch();
   const { closeModal } = useModal();
 
+  const prodInfo = useSelector(state => state.products.productDetail)
+  const currDateObj = new Date()
+  const dateOnly = currDateObj.toISOString().split('T')[0];
+
+  console.log('the prod info', prodInfo)
   const [review, setReview] = useState("");
-  const [star_rating, setStar_rating] = useState("");
+  // const [star_rating, setStar_rating] = useState("");
   const [image, setImage] = useState("");
   const [errors, setErrors] = useState({});
   const [isDisabled, setDisabled] = useState(true);
+
+  let [stars, setStars] = useState(null);
+  const [hover, setHover] = useState(null);
 
   const errorCollector = {};
 
@@ -26,9 +37,9 @@ function ReviewFormModal({ productId }) {
       errorCollector.review = "Review has only spaces";
     }
 
-    if (star_rating === "") {
+    if (stars === "") {
       errorCollector.stars = "Star Rating Required";
-    } else if (star_rating < 1 || star_rating > 5 || !parseInt(star_rating)) {
+    } else if (stars < 1 || stars > 5 || !parseInt(stars)) {
       errorCollector.stars = "Invalid input for stars (must be between 1 - 5)";
     }
     if (image && !validFormats.includes(image.toLowerCase().slice(-4))) {
@@ -42,7 +53,7 @@ function ReviewFormModal({ productId }) {
     } else {
       setDisabled(false);
     }
-  }, [review, star_rating, image]);
+  }, [review, image]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -50,7 +61,7 @@ function ReviewFormModal({ productId }) {
     try {
       const newReview = {
         review: review,
-        star_rating: star_rating,
+        star_rating: stars,
         image_url: image,
       };
 
@@ -65,44 +76,79 @@ function ReviewFormModal({ productId }) {
 
   return (
     <>
-      <h1>Leave a Review</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Review
-          <input
-            type="text"
-            value={review}
-            onChange={(e) => setReview(e.target.value)}
-            required
-          />
-        </label>
-        {errors && errors.review && <p className="errorDiv">{errors.review}</p>}
-        <label>
-          Stars
-          <input
-            type="number"
-            value={star_rating}
-            onChange={(e) => setStar_rating(e.target.value)}
-            required
-          />
-        </label>
-        {errors && errors.stars && <p className="errorDiv">{errors.stars}</p>}
-        <label>
-          Images
-          <input
-            type="text"
-            value={image}
-            onChange={(e) => setImage(e.target.value)}
-            placeholder="Optional"
-          />
-        </label>
-        {errors && errors.rev_image && (
-          <p className="errorDiv">{errors.rev_image}</p>
-        )}
-        <button type="submit" disabled={isDisabled}>
-          Submit a Review
-        </button>
-      </form>
+    <div className="reviews-modal-wrapper">
+      <h1 className="reviews-modal-h1">Leave a Review</h1>
+      <div className="reviews-modal-top">
+        <div className="reviews-modal-product-img-div">
+          <img src={prodInfo.preview_image_url} alt={prodInfo.name}/>
+        </div>
+        <div className="reviews-modal-product-info-div">
+          <h3>{prodInfo.name}</h3>
+          <p style={{color: "#A3A3A3"}}>Purchased from <span style={{color: "#595959"}}>{prodInfo.Seller.first_name}store</span> on {dateOnly}</p>
+        </div>
+      </div>
+
+        <form onSubmit={handleSubmit}>
+        <label> My review rating <span style={{color:'#A61A2D'}}>*</span></label>
+
+            {/* <input
+              type="number"
+              value={star_rating}
+              onChange={(e) => setStar_rating(e.target.value)}
+              required
+            /> */}
+
+          <div className="reviews-modal-stars-div">
+              {[...Array(5)].map((star, i) => {
+              const ratingValue = i + 1;
+              console.log(stars, ':the current rating')
+              return (
+                  <label key={i}>
+                      <input
+                          type='radio'
+                          name='rating'
+                          value={ratingValue}
+                          onClick={() => setStars(ratingValue)}
+                          />
+                      <FaStar
+                          className='star'
+                          color={ratingValue <= (hover || stars) ? "#ffc107" : "#e4e5e9"}
+                          size={20}
+                          onMouseEnter={() => setHover(ratingValue)}
+                          onMouseLeave={() => setHover(null)}
+                          />
+                  </label>
+                  )
+              })}
+          </div>
+          {errors && errors.stars && <p className="errorDiv">{errors.stars}</p>}
+          <label>
+            Your review <span style={{color:'#A61A2D'}}>*</span>
+            <input
+              type="text"
+              value={review}
+              onChange={(e) => setReview(e.target.value)}
+              required
+            />
+          </label>
+          {errors && errors.review && <p className="errorDiv">{errors.review}</p>}
+          <label>
+            Images
+            <input
+              type="text"
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+              placeholder="Optional"
+            />
+          </label>
+          {errors && errors.rev_image && (
+            <p className="errorDiv">{errors.rev_image}</p>
+          )}
+          <button type="submit" disabled={isDisabled}>
+            Submit a Review
+          </button>
+        </form>
+    </div>
     </>
   );
 }
