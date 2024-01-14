e5f99ad07c5a16bd361a8080b9d5f882b5721a7d
Will Torres <w.torresjr@gmail.com>
2024-01-14 13:51:40 -0500

    removed console.logs

--- react-app/src/components/Product-Components/ProductTileV2/index.js
+++ react-app/src/components/Product-Components/ProductTileV2/index.js
@@ -1,31 +1,3 @@
-// import "./product-tile-v2.css";
-// import "../DynaProductDisplay/dynamic-product-display.css";
-// import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
-// import { useState, useEffect } from "react";
-
-// const ProductTileV2 = ({ product, isFavorite }) => {
-//   const [productId, setProductId] = useState();
-//   useEffect(() => {
-//     if (isFavorite === "true") {
-//       // console.log(product, isFavorite, "Current Item");
-//       setProductId(product?.product_id);
-//     } else {
-//       setProductId(product?.id);
-//     }
-//   }, [product, isFavorite, productId]);
-
-//   return (
-//     <div className="dynaItem">
-//       <NavLink to={`/products/${productId}`}>
-//         {console.log(product, isFavorite, "PRODUCT INSIDE NAVLINK")}
-//         <img src={product?.preview_image_url}></img>
-//       </NavLink>
-//     </div>
-//   );
-// };
-
-// export default ProductTileV2;
-
 import "./product-tile-v2.css";
 import "../DynaProductDisplay/dynamic-product-display.css";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
