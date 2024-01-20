140c037c0296671b92408c4243d848d2521a719f
Krystal Kimmel <kabkimmel@gmail.com>
2024-01-19 19:26:34 -0600

    viewed file

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -4,6 +4,16 @@ from flask_login import current_user, login_required
 
 reviews_routes = Blueprint('reviews', __name__)
 
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
 
 # Edit a Review By Review Id
 
