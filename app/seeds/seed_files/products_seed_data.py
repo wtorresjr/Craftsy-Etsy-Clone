5961275aa6b36b8cfda1b4f4d5779873a6f91c54
Will Torres <w.torresjr@gmail.com>
2024-02-10 17:29:39 -0500

    Added AWS image urls as property to product seed data

--- app/seeds/seed_files/products_seed_data.py
+++ app/seeds/seed_files/products_seed_data.py
@@ -1,317 +1,314 @@
 products_data = [
-    
     {
-            "name": "Canvas Art",
-            "description": "Abstract canvas art. Hand-painted. 24x36 inches.",
-            "quantity": 20,
-            "price": 79.99,
-            "user_id": 3
+        "name": "Canvas Art",
+        "description": "Abstract canvas art. Hand-painted. 24x36 inches.",
+        "quantity": 20,
+        "price": 79.99,
+        "user_id": 3,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/canvas-art.jpeg",
     },
     {
-        
-            "name": "Bluetooth Headphones",
-            "description": "Wireless over-ear headphones. Noise-canceling feature.",
-            "quantity": 15,
-            "price": 129.95,
-            "user_id": 2
-
+        "name": "Bluetooth Headphones",
+        "description": "Wireless over-ear headphones. Noise-canceling feature.",
+        "quantity": 15,
+        "price": 129.95,
+        "user_id": 2,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/bluetooth-headphones.jpeg",
     },
     {
-        
-            "name": "Gaming Mouse",
-            "description": "RGB gaming mouse with customizable buttons.",
-            "quantity": 30,
-            "price": 49.99,
-            "user_id": 1
-
+        "name": "Gaming Mouse",
+        "description": "RGB gaming mouse with customizable buttons.",
+        "quantity": 30,
+        "price": 49.99,
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/gaming-mouse.jpeg",
     },
     {
-        
-            "name": "Handmade Leather Wallet",
-            "description": "Genuine leather wallet with card slots and coin pocket.",
-            "quantity": 25,
-            "price": 34.95,
-            "user_id": 2
-
+        "name": "Handmade Leather Wallet",
+        "description": "Genuine leather wallet with card slots and coin pocket.",
+        "quantity": 25,
+        "price": 34.95,
+        "user_id": 2,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/handmade-leather-wallet.jpeg",
     },
     {
-        
-            "name": "Scented Candle Set",
-            "description": "Set of 3 soy wax candles in assorted scents.",
-            "quantity": 40,
-            "price": 22.99,
-            "user_id": 1
-
+        "name": "Scented Candle Set",
+        "description": "Set of 3 soy wax candles in assorted scents.",
+        "quantity": 40,
+        "price": 22.99,
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/scented-candle-set.jpeg",
     },
     {
-        
-            "name": "Plant Pot Collection",
-            "description": "Set of 5 ceramic plant pots in varying sizes.",
-            "quantity": 18,
-            "price": 45.00,
-            "user_id": 2
- 
+        "name": "Plant Pot Collection",
+        "description": "Set of 5 ceramic plant pots in varying sizes.",
+        "quantity": 18,
+        "price": 45.00,
+        "user_id": 2,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/plant-pot-collection.jpeg",
     },
     {
-        
-            "name": "Smart Fitness Tracker",
-            "description": "Water-resistant fitness tracker with heart rate monitor.",
-            "quantity": 22,
-            "price": 79.99,
-            "user_id": 1
-
+        "name": "Smart Fitness Tracker",
+        "description": "Water-resistant fitness tracker with heart rate monitor.",
+        "quantity": 22,
+        "price": 79.99,
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/smart-fitness-tracker.jpeg",
     },
     {
-        
-            "name": "Wooden Chess Set",
-            "description": "Handcrafted wooden chess set with storage.",
-            "quantity": 12,
-            "price": 64.95,
-            "user_id": 3
- 
+        "name": "Wooden Chess Set",
+        "description": "Handcrafted wooden chess set with storage.",
+        "quantity": 12,
+        "price": 64.95,
+        "user_id": 3,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/wooden-chess-set.jpeg",
     },
     {
-        
-            "name": "Eco-Friendly Lunch Box",
-            "description": "Reusable and sustainable lunch box with compartments.",
-            "quantity": 28,
-            "price": 18.50,
-            "user_id": 1
- 
+        "name": "Eco-Friendly Lunch Box",
+        "description": "Reusable and sustainable lunch box with compartments.",
+        "quantity": 28,
+        "price": 18.50,
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/eco-friendly-lunch-box.webp",
     },
     {
-        
-            "name": "Digital Sketch Tablet",
-            "description": "Graphic drawing tablet with pressure sensitivity.",
-            "quantity": 17,
-            "price": 129.99,
-            "user_id": 2
- 
+        "name": "Digital Sketch Tablet",
+        "description": "Graphic drawing tablet with pressure sensitivity.",
+        "quantity": 17,
+        "price": 129.99,
+        "user_id": 2,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/digital-sketch-tablet.jpeg",
     },
     {
-        
-            "name": "Wireless Charging Pad",
-            "description": "Qi-compatible wireless charging pad for smartphones.",
-            "quantity": 25,
-            "price": 29.99,
-            "user_id": 1
-    
+        "name": "Wireless Charging Pad",
+        "description": "Qi-compatible wireless charging pad for smartphones.",
+        "quantity": 25,
+        "price": 29.99,
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/wireless-charging-pad.jpeg",
     },
     {
-        
-            "name": "Stainless Steel Water Bottle",
-            "description": "Double-walled insulated water bottle. 20 oz.",
-            "quantity": 32,
-            "price": 19.95,
-            "user_id": 3
-       
+        "name": "Stainless Steel Water Bottle",
+        "description": "Double-walled insulated water bottle. 20 oz.",
+        "quantity": 32,
+        "price": 19.95,
+        "user_id": 3,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/stainless-steel-water-bottle.jpeg",
     },
     {
-        
-            "name": "Vintage Record Player",
-            "description": "Turntable with built-in speakers. Retro design.",
-            "quantity": 10,
-            "price": 149.99,
-            "user_id": 1
-     
+        "name": "Vintage Record Player",
+        "description": "Turntable with built-in speakers. Retro design.",
+        "quantity": 10,
+        "price": 149.99,
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/vintage-record-player.jpeg",
     },
     {
-        
-            "name": "Crossbody Camera Bag",
-            "description": "Compact camera bag with adjustable strap.",
-            "quantity": 20,
-            "price": 39.95,
-            "user_id": 2
-    
+        "name": "Crossbody Camera Bag",
+        "description": "Compact camera bag with adjustable strap.",
+        "quantity": 20,
+        "price": 39.95,
+        "user_id": 2,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/crossbody-camera-bag.jpg",
     },
     {
-        
-            "name": "Microfiber Bedding Set",
-            "description": "Soft and breathable microfiber bedding. Queen size.",
-            "quantity": 15,
-            "price": 54.99,
-            "user_id": 3
-   
+        "name": "Microfiber Bedding Set",
+        "description": "Soft and breathable microfiber bedding. Queen size.",
+        "quantity": 15,
+        "price": 54.99,
+        "user_id": 3,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/microfiber-bedding-set.jpg",
     },
     {
-        
-            "name": "Electric Tea Kettle",
-            "description": "Stainless steel electric kettle with temperature control.",
-            "quantity": 18,
-            "price": 32.50,
-            "user_id": 2
-    
+        "name": "Electric Tea Kettle",
+        "description": "Stainless steel electric kettle with temperature control.",
+        "quantity": 18,
+        "price": 32.50,
+        "user_id": 2,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/electric-tea-kettle.jpeg",
     },
     {
-        
-            "name": "Artificial Succulent Set",
-            "description": "Set of 8 lifelike artificial succulents in ceramic pots.",
-            "quantity": 27,
-            "price": 14.99,
-            "user_id": 1
-     
+        "name": "Artificial Succulent Set",
+        "description": "Set of 8 lifelike artificial succulents in ceramic pots.",
+        "quantity": 27,
+        "price": 14.99,
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/artificial-succulent-set.jpg",
     },
     {
-        
-            "name": "HD Webcam",
-            "description": "1080p HD webcam with built-in microphone for video conferencing.",
-            "quantity": 23,
-            "price": 49.99,
-            "user_id": 2
-    
+        "name": "HD Webcam",
+        "description": "1080p HD webcam with built-in microphone for video conferencing.",
+        "quantity": 23,
+        "price": 49.99,
+        "user_id": 2,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/hd-webcam.jpeg",
     },
     {
-        
-            "name": "Bamboo Cutting Board",
-            "description": "Durable bamboo cutting board with juice groove.",
-            "quantity": 35,
-            "price": 24.95,
-            "user_id": 1
-   
+        "name": "Bamboo Cutting Board",
+        "description": "Durable bamboo cutting board with juice groove.",
+        "quantity": 35,
+        "price": 24.95,
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/bamboo-cutting-board.jpg",
     },
     {
-        
-            "name": "Digital Piano",
-            "description": "88-key digital piano with weighted keys and multiple sounds.",
-            "quantity": 8,
-            "price": 599.99,
-            "user_id": 2
-   
+        "name": "Digital Piano",
+        "description": "88-key digital piano with weighted keys and multiple sounds.",
+        "quantity": 8,
+        "price": 599.99,
+        "user_id": 2,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/digital-piano.jpeg",
     },
     {
         "name": "Artisanal Soap Set",
         "description": "Handcrafted soap set with natural ingredients and various scents.",
         "quantity": 18,
         "price": 15.99,
-        "user_id": 3
+        "user_id": 3,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/artisanal-soap-set.jpeg",
     },
     {
         "name": "Wireless Earbuds",
         "description": "True wireless earbuds with touch controls and charging case.",
         "quantity": 25,
         "price": 79.95,
-        "user_id": 1
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/wireless-earbuds.jpg",
     },
     {
         "name": "Laptop Backpack",
         "description": "Durable laptop backpack with multiple compartments and USB charging port.",
         "quantity": 30,
         "price": 42.99,
-        "user_id": 2
+        "user_id": 2,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/laptop-backpack.jpg",
     },
     {
         "name": "Personal Blender",
         "description": "Single-serve personal blender for smoothies and shakes.",
         "quantity": 20,
         "price": 34.50,
-        "user_id": 3
+        "user_id": 3,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/personal-blender.jpg",
     },
     {
         "name": "Handwoven Throw Blanket",
         "description": "Cozy handwoven throw blanket with fringe details.",
         "quantity": 22,
         "price": 28.99,
-        "user_id": 1
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/throw-blanket.jpg",
     },
     {
         "name": "Desk Organizer Set",
         "description": "Set of desk organizers for pens, paper, and office supplies.",
         "quantity": 15,
         "price": 19.95,
-        "user_id": 2
+        "user_id": 2,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/desk-organizer.jpg",
     },
     {
         "name": "Portable Power Bank",
         "description": "Compact power bank with fast charging capability. 10000mAh.",
         "quantity": 28,
         "price": 24.99,
-        "user_id": 1
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/portable-power-bank.jpg",
     },
     {
         "name": "Sustainable Tote Bag",
         "description": "Reusable tote bag made from eco-friendly materials.",
         "quantity": 35,
         "price": 12.75,
-        "user_id": 3
+        "user_id": 3,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/sus-tote-bag.jpg",
     },
     {
         "name": "Smart Light Bulbs",
         "description": "Set of smart LED light bulbs with color-changing capabilities.",
         "quantity": 20,
         "price": 17.99,
-        "user_id": 1
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/smart-light-bulbs.jpg",
     },
     {
         "name": "Digital Alarm Clock",
         "description": "Modern digital alarm clock with temperature display and USB ports.",
         "quantity": 18,
         "price": 29.95,
-        "user_id": 2
+        "user_id": 2,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/digital-alarm-clock.jpg",
     },
     {
         "name": "Collapsible Water Bottle",
         "description": "BPA-free collapsible water bottle with flip-top lid. 16 oz.",
         "quantity": 32,
         "price": 14.50,
-        "user_id": 3
+        "user_id": 3,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/collapsable-water-btl.jpg",
     },
     {
         "name": "Aromatherapy Diffuser",
         "description": "Ultrasonic aromatherapy diffuser with color-changing lights.",
         "quantity": 24,
         "price": 39.99,
-        "user_id": 1
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/aromatherapy-diffuser.jpg",
     },
     {
         "name": "Indoor Plants Assortment",
         "description": "Set of 4 low-maintenance indoor plants in decorative pots.",
         "quantity": 14,
         "price": 49.95,
-        "user_id": 2
+        "user_id": 2,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/indoor-plants-assortment.jpg",
     },
     {
         "name": "Ceramic Knife Set",
         "description": "Sharp and durable ceramic knife set with ergonomic handles.",
         "quantity": 18,
         "price": 32.75,
-        "user_id": 1
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/ceramic-knife-set.jpg",
     },
     {
         "name": "Portable Espresso Maker",
         "description": "Compact and portable espresso maker for on-the-go coffee lovers.",
         "quantity": 20,
         "price": 59.99,
-        "user_id": 3
+        "user_id": 3,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/portable-espresso-maker.jpg",
     },
     {
         "name": "Himalayan Salt Lamp",
         "description": "Natural Himalayan salt lamp for ambient lighting and air purification.",
         "quantity": 15,
         "price": 27.95,
-        "user_id": 2
+        "user_id": 2,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/himalayan-salt-lamp.jpg",
     },
     {
         "name": "Minimalist Desk Chair",
         "description": "Modern and ergonomic desk chair with minimalist design.",
         "quantity": 10,
         "price": 89.99,
-        "user_id": 1
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/minimalist-chair.jpg",
     },
     {
         "name": "Fitness Resistance Bands",
         "description": "Set of 5 fitness resistance bands for home workouts.",
         "quantity": 28,
         "price": 18.99,
-        "user_id": 3
+        "user_id": 3,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/fitness-resistance-bands.jpg",
     },
     {
         "name": "Portable Photo Printer",
         "description": "Compact photo printer for instant prints from your smartphone.",
         "quantity": 22,
         "price": 79.50,
-        "user_id": 1
+        "user_id": 1,
+        "image_url": "https://craftsy-group-s3-bucket.s3.us-east-2.amazonaws.com/portable-photo-printer.jpg",
     },
-   
-
-
 ]
