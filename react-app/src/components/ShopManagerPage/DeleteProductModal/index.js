21c9d8e0bf017095153479b67ca24bc8e6f435df
Krystal <kabkimmel@gmail.com>
2024-02-24 12:55:15 -0600

    modified component to have similar structure to the delete a review modal

--- react-app/src/components/ShopManagerPage/DeleteProductModal/index.js
+++ react-app/src/components/ShopManagerPage/DeleteProductModal/index.js
@@ -7,23 +7,21 @@ function DeleteProductModal({ product }) {
     const dispatch = useDispatch();
     const { closeModal } = useModal();
 
-
-
-
-
     const handleDelete = () => {
         dispatch(deleteProduct(product.id));
         closeModal();
-    }
-
-
+    };
 
     return (
         <>
-            <div className="delete-product-container">
-                <button onClick={handleDelete}>Delete</button>
-                <button onClick={closeModal}>Cancel</button>
-            </div>
+        <div className='delete-product-modal-wrapper'>
+          <h1 className="delete-product-modal-h1">Delete a Review</h1>
+          <p>Are you sure you want to delete the product: <span style={{fontWeight:'600'}}>{product.name}</span>?</p>
+              <div className='buttons-mgmt-div'>
+                <button className='yes-button' onClick={handleDelete}>Yes</button>
+                <button className='no-button' onClick={closeModal}>No</button>
+              </div>
+        </div>
         </>
     );
 }
